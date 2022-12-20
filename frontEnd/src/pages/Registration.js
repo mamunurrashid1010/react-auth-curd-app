a25eca4f2633b41c0b780918beb282b1ce035513
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-20 15:09:32 +0600

    FrontEnd : registration page implement

--- frontEnd/src/pages/Registration.js
+++ frontEnd/src/pages/Registration.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form,Button, Container, Row, Col } from 'react-bootstrap';
+import { Form,Button, Container, Row, Col,Alert } from 'react-bootstrap';
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
 
@@ -11,7 +11,9 @@ class Registration extends Component {
                 mobile:'',
                 email:'',
                 password:'',
+                validationError:'',
                 postResult:'',
+                errorMessage:'',
         }
     }
 
@@ -26,20 +28,29 @@ class Registration extends Component {
         if(inputName === 'fullName'){
             var namePattern = /^[a-zA-Z ]{2,30}$/;
             if(!namePattern.test(inputValue)){
-                this.setState({fullName:"Name is not valid"});
+                this.setState({validationError:"Name is not valid"});
+            }
+            else{
+                this.setState({validationError:" "});
             }
         }
         // mobile validation
         if(inputName === 'mobile'){
             if(!Number(inputValue)){
-                this.setState({mobile:"Mobile is not valid"});
+                this.setState({validationError:"Mobile is not valid"});
+            }
+            else{
+                this.setState({validationError:" "});
             }
         }
         // email validation
         if(inputName === 'email'){
             var emailPattern = /^\S+@\S+\.\S+$/;
             if(!emailPattern.test(inputValue)){
-                this.setState({email:"Email is not valid"});
+                this.setState({validationError:"Email is not valid"});
+            }
+            else{
+                this.setState({validationError:" "});
             }
         }
 
@@ -55,14 +66,20 @@ class Registration extends Component {
             password:this.state.password
         };
         // post data
-        axios.post('http://127.0.0.1:8000/api/registration/saveReg',data)
+        axios.post('http://127.0.0.1:8000/api/auth/register',data)
         .then(response=>{
-            this.setState({postResult:response.data});
-            alert(response.data.message);
-            <Navigate  to="/login" />
+            if(response.status==201){
+                this.setState({postResult:response.data.message});
+            }
+            //<Navigate  to="/login" />
         })
         .catch(error=>{
-            console.log(error);
+            if(error.response.data['name'])
+                this.setState({errorMessage:error.response.data['name']})
+            if(error.response.data['email'])
+                this.setState({errorMessage:error.response.data['email']})
+            if(error.response.data['password'])
+                this.setState({errorMessage:error.response.data['password']})
         })
     }
 
@@ -71,13 +88,18 @@ class Registration extends Component {
             <div>
                 <Row>
                         <Col>
-                            <p>Name: {this.state.fullName}</p>
+                            {/* <p>Name: {this.state.fullName}</p>
                             <p>Mobile: {this.state.mobile}</p>
                             <p>Email: {this.state.email}</p>
                             <p>password: {this.state.password}</p>
+                            <p>user: {this.state.userDetails}</p> */}
                         </Col>
                         <Col>
-                        <h1 style={{paddingTop:'100px'}}>Registration</h1><br></br>
+                        <h3 style={{paddingTop:'100px'}}><Alert className='alert'>Registration</Alert></h3><br></br>
+                        <h5>{this.state.validationError}</h5>
+                        {/* server error message*/}
+                        <h5>{this.state.errorMessage}</h5>
+                        <h5>{this.state.postResult}</h5>
                         <form onSubmit={this.formSubmitHandler}>
                             <input name="fullName" type="text" placeholder="Enter your name" onChange={this.inputOnChnageHandler} style={{width:'100%'}}></input><br></br><br></br>
                             <input name="mobile" type="text" placeholder="Mobile No" onChange={this.inputOnChnageHandler} style={{width:'100%'}}></input><br></br><br></br>
