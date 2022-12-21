6e87591a78fcdf0d0fc4d42f92fb629954a2b2d9
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-21 11:32:41 +0600

    BackEnd : Login method functionalities complete

--- BackEnd/app/Http/Controllers/AuthController.php
+++ BackEnd/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -46,5 +47,45 @@ class AuthController extends Controller
             'message'=>$message,
             'user'   =>$user,
         ],201);
+    }
+
+    /*
+     * login user with jwt
+     */
+    public function login(Request $request){
+        $data=$request->all();
+
+        // validation
+        $rule=[
+            'email'     => 'required|email|exists:users',
+            'password'  => 'required',
+        ];
+        $customMessage=[
+            'email.required'    => 'Email is required',
+            'email.email'       => 'Email must be a valid mail',
+            'email.exists'      => 'Email does not exist',
+            'password.required' => 'Password is required',
+        ];
+        $validation=Validator::make($data,$rule,$customMessage);
+        if($validation->fails()){
+            return response()->json($validation->errors(),422);
+        }
+
+        if(!$token=auth()->attempt($validation->validated())){
+            return response()->json(['error'=>'Unauthorized',401]);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    /*
+     * create new token for user
+     */
+    public function createNewToken($token){
+        return response()->json([
+            'access_token'=> $token,
+            'token_type'  => 'bearer',
+            'user'        => Auth::user(),
+        ]);
     }
 }
